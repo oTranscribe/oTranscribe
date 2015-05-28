4fbad5fa47ea78846fb6bf9007adedf066d0a108
ejb <mail@elliotbentley.com>
2015-05-28 12:26:00 +0100

    Sticky message panel

--- src/js/app/message-panel.js
+++ src/js/app/message-panel.js
@@ -2,10 +2,20 @@ oT.message = {}
 
 oT.message.header = function(msg){
     $('.message-panel .message-content').html( msg );
-    $('.message-panel').removeClass('hidden');
+    var $panel = $('.message-panel');
+    $panel.removeClass('hidden');
+    oT.message.stickyWatch = setInterval(function(){
+        if ( $panel.offset().top < 30 ) {
+            $panel.css('margin-left', $panel.css('margin-left') );
+            $panel.addClass('stuck');
+        } else if (!$panel.hasClass('stuck')) {
+            $panel.removeClass('stuck');
+        }
+    },50);
 }
 
 oT.message.close = function(){
     $('.message-panel').addClass('hidden');
+    clearInterval(oT.message.stickyWatch);
 }
 
