8e0d9d4656ccff953ed7a8be6a17053d03ba4ca9
ejb <mail@elliotbentley.com>
2016-11-27 11:06:42 -0500

    Preserve line breaks when cleaning html

--- src/js/app/clean-html.js
+++ src/js/app/clean-html.js
@@ -2,7 +2,7 @@ const sanitizeHtml = require('sanitize-html');
 
 export function cleanHTML(dirty) {
     return sanitizeHtml(dirty, {
-        allowedTags: [ 'b', 'i', 'em', 'strong', 'a', 'p', 'span' ],
+        allowedTags: [ 'b', 'i', 'em', 'strong', 'a', 'p', 'span', 'br' ],
         transformTags: {
             'div': sanitizeHtml.simpleTransform('p'),
         },
