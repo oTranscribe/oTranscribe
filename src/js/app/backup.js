778a33e9d54baf9a418ca72f65b3e1c49a4640ab
ejb <mail@elliotbentley.com>
2017-03-20 20:47:58 -0400

    Handle backup corruption errors

--- src/js/app/backup.js
+++ src/js/app/backup.js
@@ -205,7 +205,15 @@ function addDocsToPanel(start,end){
 
 function listFiles(){
     var result = [];
-    var ls = localStorageManager.getArray();
+    var ls = [];
+    try {
+        ls = localStorageManager.getArray();
+    } catch (e) {
+        console.error(e);
+        console.error('Problem listing files from localStorage.');
+        showMessage('Error listing files from localStorage. Manually clearing localStorage data may fix this. <a href="./help#why_is_otranscribe_is_no_longer_saving_backups">Instructions here</a>.');
+        throw(e);
+    }
     for (var i = 0; i < ls.length; i++) {
         if (ls[i].key.indexOf('oTranscribe-backup') > -1) {
             result.push( ls[i].key );
