c5701c532e843d1e2bb191394fd99eb6df19cbde
Naomi Pentrel <5212232+npentrel@users.noreply.github.com>
2018-10-30 10:22:11 -0700

    Add character line counts

--- src/js/app/texteditor.js
+++ src/js/app/texteditor.js
@@ -10,33 +10,65 @@ function countWords(str){
     return 0;
 }
 
+function countChars(str){
+    var chars = 0;
+    if (document.getSelection().baseNode) {
+      var parentElement = document.getSelection().baseNode.parentElement;
+
+      // If we have bold or italics text the parentElement will be one level up
+      if (parentElement.localName !== "p") {
+          parentElement = parentElement.parentElement;
+          // if the focus is on a popup this will reset to 0
+          if (parentElement.localName !== "p") {
+              return 0;
+          }
+      }
+
+      var childNodes = parentElement.childNodes;
+      for (var child in childNodes) {
+        if (childNodes[child].textContent) {
+          chars = chars + childNodes[child].textContent.length;
+        }
+      }
+    }
+
+    return chars;
+}
+
 function countTextbox(){
     var textboxElement = document.getElementById('textbox');
+
     var textboxText = textboxElement.innerText || textboxElement.textContent;
-    var count = countWords(textboxText);
-      
-    var wordcountText = document.webL10n.get('wordcount', {n: count});
+    var wordcount = countWords(textboxText);
+    var charcount = countChars(textboxText);
+
+    var wordcountText = document.webL10n.get('wordcount', {n: wordcount});
     wordcountText = wordcountText.replace(/(\d+)/, (n) => {
         return `<span class="word-count-number">${n}</span>`;
     });
+
+    var charcountText = document.webL10n.get('charcount', {n: charcount});
+    charcountText = charcountText.replace(/(\d+)/, (n) => {
+        return `<span class="char-count-number">${n}</span>`;
+    });
+
     document.querySelector('.wc-text').innerHTML = wordcountText;
+    document.querySelector('.cc-text').innerHTML = charcountText;
 }
 
-function initWordCount(){
+function initCounts(){
     countTextbox();
     setInterval(function(){
         countTextbox();
     }, 1000);
-    
 }
-
 
 function watchFormatting(){
     var b = document.queryCommandState("Bold");
     var bi = document.getElementById("icon-b");
     var i = document.queryCommandState("italic");
     var ii = document.getElementById("icon-i");
-    
+
     if (b === true){
         bi.className = "fa fa-bold active"
     } else {
@@ -56,27 +88,27 @@ function initWatchFormatting(){
 }
 
 function setEditorContents( dirtyText, opts = {} ) {
-    
+
     const newText = cleanHTML(dirtyText);
 
     var $textbox = $("#textbox");
-    
+
     function replaceText() {
         if (typeof newText === 'string') {
             $textbox[0].innerHTML = newText;
         } else {
             textbox[0].innerHTML = '';
-            $textbox[0].appendChild(newText);    
+            $textbox[0].appendChild(newText);
         }
         activateTimestamps();
         $('.textbox-container').scrollTop(0);
     }
-    
+
     if (opts.transition) {
         $textbox.fadeOut(300,function(){
             replaceText();
             $(this).fadeIn(300);
-        });        
+        });
     } else {
         replaceText();
     }
@@ -104,7 +136,7 @@ function initAutoscroll() {
 
 export {
     initWatchFormatting as watchFormatting,
-    initWordCount as watchWordCount,
+    initCounts as watchCounts,
     setEditorContents as setEditorContents,
     initAutoscroll as initAutoscroll
 };
